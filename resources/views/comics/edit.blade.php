20e12277b0b60824000a0cf9e9e60a102c269f3e
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-12 00:15:16 +0100

    add metho PUT

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,30 +12,30 @@
 
     <div class="form-group">
         <label for="title">Titolo</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Inserisci il Titolo">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Inserisci il Titolo" value="{{$comic["title"]}}">
     </div>
 
     <div class="form-group">
         <label for="thumb">Imagine</label>
-        <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Cerca una Copertina">
+        <input type="text" class="form-control" id="thumb" name="thumb" placeholder="Cerca una Copertina" value="{{$comic["thumb"]}}">
     </div>
 
     <div class="form-group">
         <label for="price">Prezzo fumetto</label>
-        <input type="number" class="form-control" id="price" name="price" placeholder="Prezzo">
+        <input type="number" class="form-control" id="price" name="price" placeholder="Prezzo" value="{{$comic["price"]}}">
     </div>
 
     <div class="form-group">
         <label for="date">Data di uscita</label>
-        <input type="date" class="form-control" id="date" name="date">
+        <input type="date" class="form-control" id="date" name="date" value="{{$comic["date"]}}">
     </div>
 
     <div class="form-group">
         <label for="type">Genere Comics</label>
-        <input type="type" class="form-control" id="type" name="type" placeholder="Cerca per Genere">
+        <input type="type" class="form-control" id="type" name="type" placeholder="Cerca per Genere" value="{{$comic["type"]}}">
     </div>
 
-    <button type="submit" class="btn btn-primary">Inserisci</button>
+    <button type="submit" class="btn btn-danger">Modifica</button>
     </form>
 
         
